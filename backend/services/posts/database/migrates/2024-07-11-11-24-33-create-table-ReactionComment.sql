d7615485457341688e93bca114172fd258130b58
mouhamsylla <syllamouhamed99@gmail.com>
2024-07-12 12:01:13 +0000

    database file removed

--- backend/services/posts/database/migrates/2024-07-11-11-24-33-create-table-ReactionComment.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS ReactionComment (
-	Value TEXT ,
-	CommentId INTEGER ,
-	UserId INTEGER ,
-	FOREIGN KEY (CommentId) REFERENCES Comments (Id)
-)
--- backend/services/posts/database/migrates/2024-07-11-11-24-33-create-table-ReactionPost.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS ReactionPost (
-	Value TEXT ,
-	PostId INTEGER ,
-	UserId INTEGER ,
-	FOREIGN KEY (PostId) REFERENCES Post (Id)
-)
